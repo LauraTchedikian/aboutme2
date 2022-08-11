a89b4ebbb0723a09b0c51da0fe1221f47a8c9a1c
laura <l.tchedikian25@ejm.org>
2022-08-11 14:32:57 +0200

    Initial Commit

---
+++ aboutme2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  aboutme2
+//
+//  Created by Laura on 11/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
